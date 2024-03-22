d617a2b1780ec690e9d0c1b1d9bbad995735fba2
aydecode <158396423+aydecode@users.noreply.github.com>
2024-03-22 08:50:03 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "阿远的公益GPT",
+  description: "如需体验付费模型,请联系qq:1090707415.",
   viewport: {
     width: "device-width",
     initialScale: 1,
